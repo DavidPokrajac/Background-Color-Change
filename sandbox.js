9d486feac49f9cbdee6d72f0741eeb522aeb1bbb
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-06 15:17:07 +0200

    Changed file name

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+  var button = document.querySelector("button");
+  var body = document.querySelector("body");
+
+  var colors = ["aquamarine", "steelblue", "tomato" "cornflowerblue", "limegreen", "peachpuff"];
+
+  button.addEventListener("click", function(){
+
+    var colorIndex = parseInt(Math.random() * colors.length);
+
+    body.style.backgroundColor = colors[colorIndex];
+
+  });
+
+}
